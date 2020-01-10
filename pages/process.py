67d237b6f7c79f1b586b9631d92fe4666b6a80aa
rselent <rselent@ufl.edu>
2020-01-10 16:23:44 -0500

    Update process.py

--- pages/process.py
+++ pages/process.py
@@ -24,8 +24,18 @@ column1 = dbc.Col(
             I was originally attracted to doing this project when I stumbled across 
             an entry on Kaggle. Another user had done something similar, and the
             dataset seemed quite interesting. I had spent 1-2 weeks investigating,
-            exploring, and cleaning the dataset -- utilizing both domain knowledge I 
-            had already possessed and active research --  
+            exploring, and cleaning their dataset -- utilizing both domain knowledge I 
+            had already possessed and active research -- which they had generated
+            utilizing Jet Propulsion Laboratories' own *Small-Body Database Search Engine*.
+            
+            After that initial time, I had found aspects of that dataset inadequate, so I
+            decided to try generating my own dataset from JPL. Utilizing the same
+            features as the Kaggle set, and adding a couple more (what I was looking for),
+            I had found success!
+            
+            And almost 100,000 more observations!
+            
+            #### 
 
 
             """
