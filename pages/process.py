ea4c3feb6f1fa97362b4a7e3573e2be5d826c8e2
rselent <rselent@ufl.edu>
2020-07-14 14:51:06 -0400

    improved clarity of certain story beats

--- pages/process.py
+++ pages/process.py
@@ -27,14 +27,18 @@ column1 = dbc.Col(
 			will be a declaration of *my process* with this project -- my thoughts, 
 			concerns, results, how I dealt with those and various other related things, 
 			and how I connected not just with the data, but ultimately, how I bring 
-			that story to you as well.
+			that story to you as well. 
+			
+			That said, go get a snack, get some coffee, and I 
+			hope you enjoy this peek behind the proverbial curtain.
 			
 			----- 
 			
 			-----
 			"""
 			
-		    """
+			"""
+			&nbsp;
 			
 			#### Interest
 			
@@ -42,48 +46,49 @@ column1 = dbc.Col(
 			whenever I approach a project like this, I'm often inundated with 
 			creative thoughts of "*Wouldn't it be cool if...*"
 			
-			"*Wouldn't it be cool if..?*"
+			"*Wouldn't it be cool if?*"
 			
 			Wouldn't it be cool if I could create something that could help 
 			with literally **saving the planet**?
 			
 			I was attracted to doing this project specifically when I stumbled across 
 			a dataset on Kaggle. Another user had done something similar, and the
-			dataset seemed quite interesting (and quite new). I had spent 1-2 weeks investigating,
-			exploring, and cleaning their dataset -- combining both domain knowledge I 
-			had already possessed and active research -- which they had generated
-			utilizing Jet Propulsion Laboratories' own *Small-Body Database Search Engine*.
+			dataset seemed quite interesting (and quite new). Combining both domain knowledge
+			I had already possessed and active, ongoing research, I spent a few days 
+			investigating, exploring, and cleaning their dataset, which they had generated
+			utilizing NASA Jet Propulsion Laboratory's *Small-Body Database Search Engine*.
 			
-			Through my exploration and investigation, I began to feel aspects of that dataset 
-			were becoming inadequate, so I decided to try generating my own dataset from JPL. 
-			Through selecting the same features as the baseline Kaggle set and adding 
-			the few more I was searching for, I had found success!
+			Through my exploration and investigation, I began to feel aspects of their dataset 
+			were becoming inadequate, so I decided to try generating my own dataset directly from 
+			NASA-JPL's Small Body Database. Through selecting the same features as the baseline 
+			Kaggle set and adding the few more I was searching for, I had found success!
 			
 			And almost 100,000 more observations, too!
 			
 			In total, I had generated a dataset that contained almost 1,000,000 observations 
 			across over 30 features -- quite the jump from the last project I had done, 
 			which had an absolutely-massive-at-the-time *50,000* observations. 
-			Coincidentally, that previous  project had also been my *first* 
+			Coincidentally, that previous project had also been my *first* 
 			actual data science project ever. Quite the jump, indeed.
 			
 			-----
 			"""
 			
 			"""
+			&nbsp;
 			
 			#### Incite
 			
 			After re-exploring my shiny, new, *hot-off-the-presses* dataset, direct 
-			from the venerable *JPL* and packed with with new data like *1-sigma 
+			from the venerable JPL and packed with with new data like *1-sigma 
 			uncertainty values*, I had unfortunately surmised that I'll still have 
 			to drop about half of its features. *Womp womp*.
 			
 			To be fair, most of these features had massively incomplete data, on the 
 			order of 80% NaN values. One feature even had just 8 (*eight*) real values 
-			recorded. Out of almost 922,000 observations. I made the judgment that 
-			perhaps -- with the objective that I had in mind and the more-complete domain 
-			knowledge I now possessed -- it would be better or even acceptable for me 
+			recorded. Out of almost 1 million observations. With the objective that I 
+			had in mind and the more-complete domain knowledge I now possessed, I made 
+			the judgment that perhaps it would be better or even acceptable for me 
 			to drop these features entirely, instead of imputing or trying to fill in 
 			such wholly incomplete data and potentially tainting the following process 
 			or, worse, the results. 
@@ -107,20 +112,20 @@ column1 = dbc.Col(
 			them are actually classified as Potentially Hazardous?? I knew space was big 
 			(*spaaaaaaaace*), but that still shocked me. For a brief moment, I reflected 
 			on those 40,000 observations I set aside and thought, "*What if*." 
-			My reflection broke quickly though, thankfully (lest I revisit that moral 
-			conundrum). Clearly I'd need another way to evaluate my models, as it's 
-			just too heavily imbalanced.
+			Thankfully, my reflection quickly broke, lest I revisit that moral conundrum. 
+			Clearly I'd need another way to evaluate my models, as it's just too heavily imbalanced.
 
 			-----
 			"""
 			
 			"""
+			&nbsp;
 			
 			#### Inquire
 			
 			The project rubric required that I build / fit / train a linear model, 
-			regardless of my confidence in how much it wouldn't produce desirable 
-			results... with this dataset, I mean. Sure enough, the best accuracy 
+			regardless of my lack of confidence in its ability to produce desirable 
+			results with this dataset. Sure enough, the best accuracy 
 			score I was able to produce with a linear Logistic Regression model was 
 			approximately 99.76% -- close to being a perfect complement to my baseline. 
 			Interesting, but unhelpful. I felt compelled to inspect the feature 
@@ -130,7 +135,7 @@ column1 = dbc.Col(
 			Minimum Orbit Intersection Distance, absolute magnitude (H), or its orbital 
 			condition code were weighted *negatively*. What?? Those latter features 
 			should be far more important than that!
-            
+
 			Naturally, this evoked more curiosity and research, and following along 
 			with the rubric, I constructed a pipeline to fit a Random Forest model -- it's 
 			another model we were explicitly taught how to use after all, so there should 
@@ -147,23 +152,25 @@ column1 = dbc.Col(
 			a little. 
 
 			-----
-            """
+			"""
 			
 			"""
-						
+			&nbsp;
+			
 			#### Incept
 			
 			It started with research -- a *lot* of research. This was a horrible decision 
-			vis-a-vis time management during a build week, but I felt I had no other choice. 
-			I could've accepted my fate (or rather, the fate of my predictive models thus far), 
-			but if that was truly to be my fate with this project, I was going to make Fate work 
-			*damn hard* to get her way. So, as a method to confirm the model behaviours that I've 
+			with regards to time management, given that we are only allotted a week to build this project, 
+			but I felt I had no other choice. I could've accepted my fate (or rather, 
+			the fate of my predictive models thus far), but if that was truly to be 
+			my fate with this project, I was going to make Fate work *damn hard* to 
+			get her way. So, as a method to confirm the model behaviours that I've 
 			been seeing so far, I chose *another* linear model: Perceptron. Up until now, 
 			I haven't been altering or forcing parameters all that much -- outside of n_jobs 
 			(for expedience), random_state (for reproducibility), and such -- because my 
 			guiding principle for this project was "find a model that fits my needs first," 
 			as every other dataset that I've worked with while here at Lambda School has 
-			had relatively balanced prediction targets and been, well, comparatively "easy." 
+			had relatively balanced prediction targets and has thus been, well, comparatively "easy." 
 			
 			Surprisingly, Perceptron produced the same sort of results that my previous 
 			two attempts had thrown at me, except this model was perfectly, right on the money: 
@@ -175,11 +182,10 @@ column1 = dbc.Col(
 			a bit more minority- or recall-friendly -- like Precision-Recall or 
 			F-scores -- they also suggested I plot ~~to take over the world~~ 
 			a confusion matrix, to see what's really going on beneath the surface; 
-			to see what kind of predictions this model was making (and by my own 
-			presumption, my previous models as well judging by their similar accuracy 
-			scores). It's probably better to just *show* you the results.
+			to see what kind of predictions this and the rest of my models were truly making. 
+			It's probably better to just *show* you the results.
 			
-			< Confusion atrix goes here, because words just can't describe >
+			< Confusion matrix goes here, because words just can't describe >
 			
 			Just like Owen Wilson, "*Wow*," was my first reaction. I mean, It's not 
 			making *any* 'yes' predictions. At all.	And in that moment I truly 
@@ -214,6 +220,18 @@ column1 = dbc.Col(
 #			
 			
 			"""
+			
+			#### Intuition
+			
+			I've been playing video games for a long time -- ever since I was an infant, 
+			basically. And as a gamer, my interest in data science before joining 
+			Lambda School has kind of been casual and perfunctory, as for a time, 
+			machine learning and gaming had	kind of a big intersection. Through a 
+			push from GPU manufacturers AMD (then ATI) and Nvidia towards 
+			building what are called General-Purpose GPUs, 
+			
+			
+			
 			-----
 			"""
 
