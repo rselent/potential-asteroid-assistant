395e999c8672f678898f16b59845c3cf2ba94673
rselent <rselent@ufl.edu>
2020-07-14 16:05:14 -0400

    Update run.py

--- run.py
+++ run.py
@@ -11,17 +11,17 @@ from pages import index, predictions, insights, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
-    brand= 'Potentially Hazardous Asteroid Assistant',
-    brand_href= '/',
-    children= [
-        dbc.NavItem( dcc.Link( 'Predictions', href= '/predictions', className= 'nav-link')),
-        dbc.NavItem( dcc.Link( 'Insights', href= '/insights', className= 'nav-link')),
-        dbc.NavItem( dcc.Link( 'Process', href= '/process', className= 'nav-link')),
-    ],
-    sticky= 'top',
-    color= 'light',
-    light= True,
-    dark= False
+	brand= 'Potentially Hazardous Asteroid Assistant',
+	brand_href= '/',
+	children= [
+		dbc.NavItem( dcc.Link( 'Predictions', href= '/predictions', className= 'nav-link')),
+		dbc.NavItem( dcc.Link( 'Insights', href= '/insights', className= 'nav-link')),
+		dbc.NavItem( dcc.Link( 'Process', href= '/process', className= 'nav-link')),
+	],
+	sticky= 'top',
+	color= 'light',
+	light= True,
+	dark= False
 )
 
 # Footer docs:
@@ -31,20 +31,20 @@ navbar = dbc.NavbarSimple(
 # mr (margin right) : https://getbootstrap.com/docs/4.3/utilities/spacing/
 # className='lead' : https://getbootstrap.com/docs/4.3/content/typography/#lead
 footer = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            html.P(
-                [
-                    html.Span( 'Raymond Selent', className= 'mr-2'),
-                    html.A( html.I( className= 'fas fa-envelope-square mr-1'), href= 'mailto:rselent@ufl.edu'),
-                    html.A( html.I( className= 'fab fa-github-square mr-1'), href= 'https://github.com/rselent/lambda-buildweek2-asteroids'),
-                   # html.A( html.I( className= 'fab fa-linkedin mr-1'), href= 'https://www.linkedin.com/in/<you>/'),
-                   # html.A( html.I( className= 'fab fa-twitter-square mr-1'), href= 'https://twitter.com/<you>'),
-                ], 
-                className= 'lead'
-            )
-        )
-    )
+	dbc.Row(
+		dbc.Col(
+			html.P(
+				[
+					html.Span( 'Raymond Selent', className= 'mr-2'),
+					html.A( html.I( className= 'fas fa-envelope-square mr-1'), href= 'mailto:rselent@ufl.edu'),
+					html.A( html.I( className= 'fab fa-github-square mr-1'), href= 'https://github.com/rselent/lambda-buildweek2-asteroids'),
+					html.A( html.I( className= 'fab fa-linkedin mr-1'), href= 'https://www.linkedin.com/in/rselent/'),
+					# html.A( html.I( className= 'fab fa-twitter-square mr-1'), href= 'https://twitter.com/<you>'),
+				], 
+				className= 'lead'
+			)
+		)
+	)
 )
 
 # Layout docs:
@@ -52,29 +52,29 @@ footer = dbc.Container(
 # dcc.Location: https://dash.plot.ly/dash-core-components/location
 # dbc.Container: https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 app.layout = html.Div([
-    dcc.Location( id= 'url', refresh= False),
-    navbar, 
-    dbc.Container( id= 'page-content', className= 'mt-4'),
-    html.Hr(), 
-    footer
+	dcc.Location( id= 'url', refresh= False),
+	navbar, 
+	dbc.Container( id= 'page-content', className= 'mt-4'),
+	html.Hr(), 
+	footer
 ])
 
 
 # URL Routing for Multi-Page Apps: https://dash.plot.ly/urls
 @app.callback( Output( 'page-content', 'children'),
-              [Input( 'url', 'pathname')])
+			  [Input( 'url', 'pathname')])
 def display_page( pathname):
-    if pathname == '/':
-        return index.layout
-    elif pathname == '/predictions':
-        return predictions.layout
-    elif pathname == '/insights':
-        return insights.layout
-    elif pathname == '/process':
-        return process.layout
-    else:
-        return dcc.Markdown( '## Page not found')
+	if pathname == '/':
+		return index.layout
+	elif pathname == '/predictions':
+		return predictions.layout
+	elif pathname == '/insights':
+		return insights.layout
+	elif pathname == '/process':
+		return process.layout
+	else:
+		return dcc.Markdown( '## Page not found')
 
 # Run app server: https://dash.plot.ly/getting-started
 if __name__ == '__main__':
-    app.run_server( debug= True)
+	app.run_server( debug= True)
